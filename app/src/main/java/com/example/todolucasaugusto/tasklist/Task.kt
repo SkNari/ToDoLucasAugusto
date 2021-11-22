fd2936eb7bda596965f3ae8b14aee17f05c95435
SkNari <luaugusto@laposte.net>
2021-11-22 17:53:51 +0100

    OUI BIEN JOUE

---
+++ app/src/main/java/com/example/todolucasaugusto/tasklist/Task.kt
@@ -0,0 +1,5 @@
+package com.example.todolucasaugusto.tasklist
+
+data class Task(val id:String,val title:String,val description:String = ""){
+
+}
---
+++ app/src/main/java/com/example/todolucasaugusto/tasklist/TaskListAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.todolucasaugusto.tasklist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolucasaugusto.R
+
+class TaskListAdapter(private val taskList: List<Task>) : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(taskTitle: Task) {
+            val view = itemView.findViewById<TextView>(R.id.task_title);
+            view.text = taskTitle.title;
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false);
+        return TaskViewHolder(itemView);
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(taskList[position]);
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size;
+    }
+}
